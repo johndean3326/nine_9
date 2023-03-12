65e684bf7c0b0256c35485477a493e5109b6c828
johndean3326 <113262365+johndean3326@users.noreply.github.com>
2023-03-11 22:05:03 -0700

    Update generateMarkdown.js
    markdown

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,39 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+   if (license === 'No License') {
+  return ""
+} else {
+  const badges = {
+  MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+  ISC: '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)',
+  GNUGPLv3: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+}
+return badges[license]
+}}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { if (license === 'No License') {
+  return ""
+} else {
+  const links = {
+  MIT: '[MIT](https://choosealicense.com/licenses/mit/)',
+  ISC: '[ISC](https://choosealicense.com/licenses/isc/)',
+  GNUGPLv3: '[GNUGPLv3](https://choosealicense.com/licenses/gpl-3.0/)'
+  }
+  return links[license]
+}}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { if (license === 'No License') {
+  return ""
+} else {
+  return `## License 
+  ${license}`
+}
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
